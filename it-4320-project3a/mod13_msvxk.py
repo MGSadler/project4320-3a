9b15e572cf900750f103853eddb8cc3064c46dae
MGSadler <47876138+MGSadler@users.noreply.github.com>
2020-11-20 17:11:10 -0600

    Add files via upload

---
+++ it-4320-project3a/mod13_msvxk.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestStockVisualizer(unittest.TestCase):
+
+    def test_symbol(self):
+        self.assertEqual(userInput1, .upper())
+        self.assertCountEqual(userInput1 <= 7)
+
+    def chart(self):
+        self.assertEqual(userInput2, 1 or 2)
+
+    def timeSeries(self):
+        self.assertEqual(userInput3, 1 or 2 or 3 or 4)
+
+    def start(self):
+        self.assertIs(begin_date, YYYY-MM-DD)
+
+    def end(self):
+        self.assertIs(end_date, YYYY-MM-DD)
